7273e8b34fbe9b9d08874cb68180720283e29cda
Carlos <106010038+CarlosAprillio2002@users.noreply.github.com>
2022-12-15 21:56:41 +0700

    Add files via upload

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "YOURDEPLOYURL" // TODO: replace this
+const studentName = "CARLOS APRILLIO" // TODO: replace this
+const studentId = "2640762" // TODO: replace this
+
+export { url, studentName, studentId }
